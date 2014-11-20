bdbfceb9a0f0d9192b2a360cb19f1d77acbaaad4
Christian Strobel <christian.strobel@iis.fraunhofer.de>
2014-11-20 18:29:09 +0100

    test: fhg: Add test for OGRP with position calculation
    Change-Id: I6699e12cdca9acaec12f7a909edd2cda4a79db0a

--- test/fhg/position_test.sh
+++ test/fhg/position_test.sh
@@ -6,8 +6,9 @@ LOG_NAME="${LOG_NAME:-"novatel"}"
 LATITUDE="${LATITUDE:-"45.065128652"}"
 LONGITUDE="${LONGITUDE:-"7.659363180"}"
 FORMAT="${FORMAT:-"nov"}"
+EXTENSION="${EXTENSION:-".out"}"
 
-$APP_PATH/convbin -r $FORMAT $IN_PATH/$LOG_NAME.out
+$APP_PATH/convbin -r $FORMAT $IN_PATH/$LOG_NAME$EXTENSION
 $APP_PATH/rnx2rtkp -p 0 $LOG_NAME.nav $LOG_NAME.obs > $LOG_NAME.pos
 
 latitude=$(tail -n 1 $LOG_NAME.pos | tr -s ' ' | cut -d ' ' -f 3)
