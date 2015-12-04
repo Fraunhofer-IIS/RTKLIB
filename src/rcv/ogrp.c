30ad6daf15c6e77e205d5a6009fc37299cc98e25
Christian Strobel <christian.strobel@iis.fraunhofer.de>
2015-12-04 11:32:40 +0100

    Update to OGRP v1.0
    Change-Id: I3900233d9ba9da97b68dbdcfffa492f459562ea8

--- src/rcv/ogrp.c
+++ src/rcv/ogrp.c
@@ -6,31 +6,6 @@
 #include <math.h>
 
 const static double gpst0[]={1980,1, 6,0,0,0};
-const static unsigned subframe_bytes_without_parity = 30;
-/* Number of bits is 2*(120-6)=228 */
-const static unsigned inav_page_bytes_without_tail = 29;
-
-const static double c_2_pow_m_5  = 0.03125;
-const static double c_2_pow_m_19 = 1.9073486328125e-6;
-const static double c_2_pow_m_29 = 1.86264514923096e-9;
-const static double c_2_pow_m_31 = 4.65661287307739e-10;
-const static double c_2_pow_m_32 = 2.3283064365386962890625e-10;
-const static double c_2_pow_m_33 = 1.16415321826935e-10;
-const static double c_2_pow_m_34 = 5.82076609134674072265625e-11;
-const static double c_2_pow_m_43 = 1.13686837721616e-13;
-const static double c_2_pow_m_46 = 1.4210854715202003717422485351563e-14;
-const static double c_2_pow_m_59 = 1.7347234759768070944119244813919e-18;
-
-
-/* Copy from src/rcv/novatel.c */
-static gtime_t adjweek(gtime_t time, double tow) {
-    double tow_p;
-    int week;
-    tow_p = time2gpst(time,&week);
-    if      (tow < tow_p - 302400.0) tow += 604800.0;
-    else if (tow > tow_p + 302400.0) tow -= 604800.0;
-    return gpst2time(week, tow);
-}
 
 static int get_value_check_type(json_object *jobj, char* key, json_object **value, json_type type) {
     trace(5,"get_value_check_type:\n");
@@ -164,7 +139,7 @@ static int decode_ogrp_ch_meas(raw_t *raw, json_object *jobj) {
         trace(2, "Discard channel measurement. Signal priority is too low for %s %s\n", gnss, signal_type);
     }
 
-    if (get_value_check_type(jobj, "pseudorange", &jpseudorange, json_type_double) < 0) return -1;
+    if (get_value_check_type(jobj, "pseudo_range", &jpseudorange, json_type_double) < 0) return -1;
     pseudorange = json_object_get_double(jpseudorange);
 
     if (get_value_check_type(jobj, "doppler", &jdoppler, json_type_double) < 0) return -1;
@@ -176,7 +151,7 @@ static int decode_ogrp_ch_meas(raw_t *raw, json_object *jobj) {
     if (get_value_check_type(jobj, "snr", &jsnr, json_type_double) < 0) return -1;
     snr = json_object_get_double(jsnr);
 
-    if (get_value_check_type(jobj, "sat_id", &jsat_id, json_type_int) < 0) return -1;
+    if (get_value_check_type(jobj, "satellite_id", &jsat_id, json_type_int) < 0) return -1;
     sat_id = json_object_get_int(jsat_id);
     if (!(sat = satno(sys, sat_id))) {
         trace(3,"satellite number error: sys=%d, sat_id=%d\n", sys, sat_id);
@@ -205,11 +180,143 @@ static int decode_ogrp_ch_meas(raw_t *raw, json_object *jobj) {
     return 1;
 }
 
-static int decode_ogrp_measurement(raw_t *raw, json_object *jobj) {
+static int decode_ogrp_ephemeris_element(raw_t *raw, json_object *jobj) {
+    eph_t eph = {0};
+
+    json_object *jgnss, *jsat_id, *jweek_number, *jephemeris_issue, *jclock_issue, *jroot_a;
+    json_object *jclock_reference, *jephemeris_reference, *jecc, *jaf_0, *jaf_1, *jaf_2;
+    json_object *jm_0, *jomega_0, *jomega_dot, *jcrc, *jcrs, *jcuc, *jcus, *jcic, *jcis;
+    json_object *jdeln, *jinc_0, *jinc_dot, *jgroup_delay, *jarg_per;
+    char *gnss;
+    int sat_id, sat, sys, week, toc;
+    double sqrtA, tow, tt;
+
+    if (json_object_get_type(jobj) != json_type_object) {
+        trace(2, "decode_ogrp_ephemeris_element: Type error, no object\n");
+        return -1;
+    }
+
+    if (get_value_check_type(jobj, "gnss", &jgnss, json_type_string) < 0) return -1;
+    gnss = json_object_get_string(jgnss);
+
+    if (strcmp(gnss, "GPS") == 0) {
+        sys = SYS_GPS;
+    } else if (strcmp(gnss, "Galileo") == 0) {
+        sys = SYS_GAL;
+        eph.code = 0; /* INAV=0, FNAV=1 */
+    } else {
+        trace(2, "Unsupported GNSS system %s\n", gnss);
+        return -1;
+    }
+
+    if (get_value_check_type(jobj, "satellite_id", &jsat_id, json_type_int) < 0) return -1;
+    sat_id = json_object_get_int(jsat_id);
+    if (!(sat = satno(sys, sat_id))) {
+        trace(3,"satellite number error: sys=%d, sat_id=%d\n", sys, sat_id);
+        return -1;
+    }
+
+    if (get_value_check_type(jobj, "ephemeris_issue", &jephemeris_issue, json_type_int) < 0) return -1;
+    eph.iode = json_object_get_int(jephemeris_issue);
+    if (eph.iode == raw->nav.eph[sat - 1].iode) return -1;
+
+    if (get_value_check_type(jobj, "clock_issue", &jclock_issue, json_type_int) < 0) return -1;
+    eph.iodc = json_object_get_int(jclock_issue);
+    if (eph.iodc == raw->nav.eph[sat - 1].iodc) return -1;
+
+    if (get_value_check_type(jobj, "root_a", &jroot_a, json_type_double) < 0) return -1;
+    sqrtA = json_object_get_double(jroot_a);
+    eph.A = sqrtA * sqrtA;
+
+    if (get_value_check_type(jobj, "ephemeris_reference", &jephemeris_reference, json_type_double) < 0) return -1;
+    eph.toes = json_object_get_double(jephemeris_reference);
+
+    if (get_value_check_type(jobj, "clock_reference", &jclock_reference, json_type_double) < 0) return -1;
+    toc = json_object_get_double(jclock_reference);
+
+    if (get_value_check_type(jobj, "week_number", &jweek_number, json_type_int) < 0) return -1;
+    week = json_object_get_int(jweek_number);
+
+    eph.week = adjgpsweek(week);
+    tow = time2gpst(raw->time, &week);
+    eph.ttr=gpst2time(eph.week,tow);
+    eph.toc=gpst2time(eph.week,toc);
+    tow=time2gpst(eph.ttr,&eph.week);
+    toc=time2gpst(eph.toc,NULL);
+    if      (eph.toes<tow-302400.0) {eph.week++; tow-=604800.0;}
+    else if (eph.toes>tow+302400.0) {eph.week--; tow+=604800.0;}
+    eph.toe=gpst2time(eph.week,eph.toes);
+    eph.toc=gpst2time(eph.week,toc);
+    eph.ttr=gpst2time(eph.week,tow);
+
+    if (get_value_check_type(jobj, "ecc", &jecc, json_type_double) < 0) return -1;
+    eph.e = json_object_get_double(jecc);
+
+    if (get_value_check_type(jobj, "af_0", &jaf_0, json_type_double) < 0) return -1;
+    eph.f0 = json_object_get_double(jaf_0);
+
+    if (get_value_check_type(jobj, "af_1", &jaf_1, json_type_double) < 0) return -1;
+    eph.f1 = json_object_get_double(jaf_1);
+
+    if (get_value_check_type(jobj, "af_2", &jaf_2, json_type_double) < 0) return -1;
+    eph.f2 = json_object_get_double(jaf_2);
+
+    if (get_value_check_type(jobj, "m_0", &jm_0, json_type_double) < 0) return -1;
+    eph.M0 = json_object_get_double(jm_0);
+
+    if (get_value_check_type(jobj, "omega_0", &jomega_0, json_type_double) < 0) return -1;
+    eph.OMG0 = json_object_get_double(jomega_0);
+
+    if (get_value_check_type(jobj, "omega_dot", &jomega_dot, json_type_double) < 0) return -1;
+    eph.OMGd = json_object_get_double(jomega_dot);
+
+    if (get_value_check_type(jobj, "crc", &jcrc, json_type_double) < 0) return -1;
+    eph.crc = json_object_get_double(jcrc);
+
+    if (get_value_check_type(jobj, "crs", &jcrs, json_type_double) < 0) return -1;
+    eph.crs = json_object_get_double(jcrs);
+
+    if (get_value_check_type(jobj, "cuc", &jcuc, json_type_double) < 0) return -1;
+    eph.cuc = json_object_get_double(jcuc);
+
+    if (get_value_check_type(jobj, "cus", &jcus, json_type_double) < 0) return -1;
+    eph.cus = json_object_get_double(jcus);
+
+    if (get_value_check_type(jobj, "cic", &jcic, json_type_double) < 0) return -1;
+    eph.cic = json_object_get_double(jcic);
+
+    if (get_value_check_type(jobj, "cis", &jcis, json_type_double) < 0) return -1;
+    eph.cis = json_object_get_double(jcis);
+
+    if (get_value_check_type(jobj, "deln", &jdeln, json_type_double) < 0) return -1;
+    eph.deln = json_object_get_double(jdeln);
+
+    if (get_value_check_type(jobj, "inc_0", &jinc_0, json_type_double) < 0) return -1;
+    eph.i0 = json_object_get_double(jinc_0);
+
+    if (get_value_check_type(jobj, "inc_dot", &jinc_dot, json_type_double) < 0) return -1;
+    eph.idot = json_object_get_double(jinc_dot);
+
+    if (get_value_check_type(jobj, "group_delay", &jgroup_delay, json_type_double) < 0) return -1;
+    eph.tgd[0] = json_object_get_double(jgroup_delay);
+
+    if (get_value_check_type(jobj, "arg_per", &jarg_per, json_type_double) < 0) return -1;
+    eph.omg = json_object_get_double(jarg_per);
+
+    /* Missing parameter eph.tgd[1] --> BGD: E5B-E1 (s) */
+
+    eph.sat = sat;
+    raw->nav.eph[sat - 1] = eph;
+    raw->ephsat = sat;
+
+    return 2;
+}
+
+static int decode_ogrp_channel_measurements(raw_t *raw, json_object *jobj) {
     json_object *ch_meas_array;
     int i, num_ch_meas, status;
 
-    trace(5,"decode_ogrp_measurement:\n");
+    trace(5,"decode_ogrp_channel_measurements:\n");
 
     status = get_value_check_type(jobj, "ch_meas", &ch_meas_array, json_type_array);
     if (status < 0) {
@@ -232,215 +339,32 @@ static int decode_ogrp_measurement(raw_t *raw, json_object *jobj) {
     return 1;
 }
 
-static int save_subfrm(int sat, raw_t *raw, unsigned char *data) {
-    unsigned char *q;
-    int i, id;
+static int decode_ogrp_ephemeris(raw_t *raw, json_object *jobj) {
+    if (raw->obs.n == 0) return -1;
+    json_object *ephem_array;
+    int i, num_ephem, status;
 
-    trace(4,"save_subfrm: sat=%2d\n", sat);
+    trace(5,"decode_ogrp_ephemeris:\n");
 
-    /* check navigation subframe preamble */
-    if (data[0] != 0x8B) {
-        trace(2,"stq subframe preamble error: 0x%02X\n", data[0]);
-        return 0;
-    }
-    id = (data[5] >> 2) & 0x7;
+    status = get_value_check_type(jobj, "ephemeris", &ephem_array, json_type_array);
+    if (status < 0) num_ephem = 0;
+    else num_ephem = json_object_array_length(ephem_array);
 
-    /* check subframe id */
-    if (id < 1 || 5 < id) {
-        trace(2,"stq subframe id error: id=%d\n", id);
-        return 0;
-    }
-    q = raw->subfrm[satno(SYS_GPS, sat) - 1] + (id - 1) * subframe_bytes_without_parity;
-
-    for (i = 0; i < subframe_bytes_without_parity; i++) q[i] = data[i];
-
-    return id;
-}
-
-static int save_page(int prn, raw_t *raw, unsigned char *data) {
-    unsigned char *q;
-    int i, type, sat;
-
-    sat = satno(SYS_GAL, prn);
-
-    trace(4,"save_page: sat=%2d\n", sat);
-
-    type = data[0] & 0x3F;
-
-    /* check word type */
-    if (type < 0 || 10 < type) {
-        trace(2,"Galileo word type error: type=%d\n", type);
-        return -1;
-    }
-
-    /* Only the ephemeris data (page 1-4) and TOW page (5) is stored and decoded, for now */
-    if (type < 1 || 5 < type) return -1;
-
-    /* Re-use the GPS subframe buffer for Galileo INAV pages */
-    q = raw->subfrm[sat - 1] + (type - 1) * inav_page_bytes_without_tail;
-    for (i = 0; i < inav_page_bytes_without_tail; i++) q[i] = data[i];
-
-    return type;
-}
-
-static int decode_ephem_gps(int sat, raw_t *raw) {
-    eph_t eph = {0};
-
-    trace(4,"decode_ephem_gps: sat=%2d\n", sat);
-
-    if (decode_frame(raw->subfrm[satno(SYS_GPS, sat) - 1] + (subframe_bytes_without_parity * 0), &eph, NULL, NULL, NULL, NULL) != 1 ||
-        decode_frame(raw->subfrm[satno(SYS_GPS, sat) - 1] + (subframe_bytes_without_parity * 1), &eph, NULL, NULL, NULL, NULL) != 2 ||
-        decode_frame(raw->subfrm[satno(SYS_GPS, sat) - 1] + (subframe_bytes_without_parity * 2), &eph, NULL, NULL, NULL, NULL) != 3) return 0;
-
-    if (eph.iode == raw->nav.eph[satno(SYS_GPS, sat) - 1].iode) return 0; /* unchanged */
-    eph.sat = satno(SYS_GPS, sat);
-    raw->nav.eph[satno(SYS_GPS, sat) - 1] = eph;
-    raw->ephsat = satno(SYS_GPS, sat);
-    return 2;
-}
-
-static int decode_ephem_galileo(int prn, raw_t *raw) {
-    eph_t eph = {0};
-    int i, j, sat, week;
-    const unsigned nr_ephem_pages_and_tow = 5;
-    const unsigned word_len_bytes = 16;
-    unsigned char words[nr_ephem_pages_and_tow][word_len_bytes];
-    unsigned char *data;
-    double sqrtA, tow, toc, tt;
-
-    sat = satno(SYS_GAL, prn);
-
-    trace(4,"decode_ephem_galileo: sat=%2d\n", sat);
-
-    for (i = 0; i < nr_ephem_pages_and_tow; i++) {
-        data = (unsigned char *) (raw->subfrm[sat - 1] + inav_page_bytes_without_tail * i);
-        /* Extract data bytes from even page part */
-        for (j = 0; j < word_len_bytes - 2; j++) words[i][j] = ((data[j] << 2) & 0xFC) | ((data[j + 1] >> 6) & 0x03);
-        /* Extract data bytes from odd page part */
-        words[i][14] = ((data[14] << 4) & 0xF0) | ((data[15] >> 4) & 0x0F);
-        words[i][15] = ((data[15] << 4) & 0xF0) | ((data[16] >> 4) & 0x0F);
-        /* Check the word type */
-        if (getbitu(words[i], 0, 6) != (i + 1)) {
-            trace(2,"Galileo word type error or incomplete ephemeris data: type=%d\n", (i + 1));
-            return -1;
+    for (i = 0; i < num_ephem; i++) {
+        json_object *ephem = json_object_array_get_idx(ephem_array, i);
+        if ((status = decode_ogrp_ephemeris_element(raw, ephem)) != 2) {
+            continue;
         }
+        break;
     }
 
-    eph.code   = 0; /* INAV=0, FNAV=1 */
-    eph.iode   = getbitu(words[1 - 1],   6, 10);
-    eph.iodc   = eph.iode;
-    eph.toes   = getbitu(words[1 - 1],  16, 14) * 60.0;
-    sqrtA      = getbitu(words[1 - 1],  94, 32) * c_2_pow_m_19;
-    eph.A      = sqrtA * sqrtA;
-    eph.e      = getbitu(words[1 - 1],  62, 32) * c_2_pow_m_33;
-    eph.i0     = getbitu(words[2 - 1],  48, 32) * c_2_pow_m_31 * PI;
-    eph.OMG0   = getbitu(words[2 - 1],  16, 32) * c_2_pow_m_31 * PI;
-    eph.omg    = getbitu(words[2 - 1],  80, 32) * c_2_pow_m_31 * PI;
-    eph.M0     = getbitu(words[1 - 1],  30, 32) * c_2_pow_m_31 * PI;
-    eph.deln   = getbitu(words[3 - 1],  40, 16) * c_2_pow_m_43 * PI;
-    eph.OMGd   = getbitu(words[3 - 1],  16, 24) * c_2_pow_m_43 * PI;
-    eph.idot   = getbitu(words[2 - 1], 112, 14) * c_2_pow_m_43 * PI;
-    eph.crc    = getbitu(words[3 - 1],  88, 16) * c_2_pow_m_5;
-    eph.crs    = getbitu(words[3 - 1], 104, 16) * c_2_pow_m_5;
-    eph.cuc    = getbitu(words[3 - 1],  56, 16) * c_2_pow_m_29;
-    eph.cus    = getbitu(words[3 - 1],  72, 16) * c_2_pow_m_29;
-    eph.cic    = getbitu(words[4 - 1],  22, 16) * c_2_pow_m_29;
-    eph.cis    = getbitu(words[4 - 1],  38, 16) * c_2_pow_m_29;
-    eph.f0     = getbitu(words[4 - 1],  68, 31) * c_2_pow_m_34;
-    eph.f1     = getbitu(words[4 - 1],  99, 21) * c_2_pow_m_46;
-    eph.f2     = getbitu(words[4 - 1], 120,  6) * c_2_pow_m_59;
-    eph.tgd[0] = getbitu(words[5 - 1],  47, 10) * c_2_pow_m_32; /* BGD: E5A-E1 (s) */
-    eph.tgd[1] = getbitu(words[5 - 1],  57, 10) * c_2_pow_m_32; /* BGD: E5B-E1 (s) */
-    eph.week   = getbitu(words[5 - 1],  73, 12);
-    toc = getbitu(words[4 - 1],  54, 14) * 60.0;
-
-    tow = time2gpst(raw->time, &week);
-    eph.week   = week;
-    eph.toe    = gpst2time(eph.week, eph.toes);
-
-    /* for week-handover problem see novatel.c*/
-    tt = timediff(eph.toe, raw->time);
-    if      (tt < -302400.0) eph.week++;
-    else if (tt >  302400.0) eph.week--;
-    eph.toe = gpst2time(eph.week, eph.toes);
-    eph.toc = adjweek(eph.toe, toc);
-    eph.ttr = adjweek(eph.toe, tow);
-
-    eph.sat = sat;
-    raw->nav.eph[sat - 1] = eph;
-    raw->ephsat = sat;
-    return 2;
-}
-
-static int decode_ogrp_raw_nav_msg_gps_l1ca(raw_t *raw, unsigned char *data, int nav_msg_id, int sat_id) {
-    int sub_id;
-    sub_id = save_subfrm(sat_id, raw, data);
-    if (sub_id != nav_msg_id) {
-        trace(2,"GPS subframe error\n");
-        return -1;
-    }
-    if (sub_id == 3) return decode_ephem_gps(sat_id, raw);
-    /* TODO decode subframe 4 and 5 */
-    return 0;
-}
-
-static int decode_ogrp_raw_nav_msg_galileo_e1b(raw_t *raw, unsigned char *data, int nav_msg_id, int sat_id) {
-    int word_type;
-    word_type = save_page(sat_id, raw, data);
-    if (word_type != nav_msg_id) {
-        trace(2,"Galileo page error\n");
-        return -1;
-    }
-    return decode_ephem_galileo(sat_id, raw);
-}
-
-static int decode_ogrp_raw_nav_msg(raw_t *raw, json_object *jobj) {
-    int sat_id, nr_bits, nav_msg_id, nr_bytes, count;
-    char *gnss, *signal_type, *data_temp;
-    json_object *jgnss, *jsignal_type, *jsat_id, *jnr_bits, *jnav_msg_id, *jdata;
-
-    trace(5,"decode_ogrp_raw_nav_msg:\n");
-
-    if (get_value_check_type(jobj, "gnss", &jgnss, json_type_string) < 0) return -1;
-    gnss = json_object_get_string(jgnss);
-
-    if (get_value_check_type(jobj, "signal_type", &jsignal_type, json_type_string) < 0) return -1;
-    signal_type = json_object_get_string(jsignal_type);
-
-    if (get_value_check_type(jobj, "nr_bits", &jnr_bits, json_type_int) < 0) return -1;
-    nr_bits = json_object_get_int(jnr_bits);
-
-    nr_bytes = (int)((float)(nr_bits) / 8.f);
-    if ((float)(nr_bits) / 8.f - (int)((float)(nr_bits) / 8.f) > 0.f) nr_bytes++;
-    unsigned char data[nr_bytes];
-
-    if (get_value_check_type(jobj, "data", &jdata, json_type_string) < 0) return -1;
-    data_temp = json_object_get_string(jdata);
-    if (strlen(data_temp) != nr_bytes * 2) return -1;
-    for(count = 0; count < nr_bytes; count++) {
-        sscanf(data_temp, "%2hhx", &data[count]);
-        data_temp += 2 * sizeof(char);
-    }
-
-    if (get_value_check_type(jobj, "nav_msg_id", &jnav_msg_id, json_type_int) < 0) return -1;
-    nav_msg_id = json_object_get_int(jnav_msg_id);
-
-    if (get_value_check_type(jobj, "sat_id", &jsat_id, json_type_int) < 0) return -1;
-    sat_id = json_object_get_int(jsat_id);
-
-    if (strcmp(gnss, "GPS") == 0 && strcmp(signal_type, "L1CA") == 0) {
-        return decode_ogrp_raw_nav_msg_gps_l1ca(raw, data, nav_msg_id, sat_id);
-    } else if (strcmp(gnss, "Galileo") == 0 && strcmp(signal_type, "E1B") == 0) {
-        return decode_ogrp_raw_nav_msg_galileo_e1b(raw, data, nav_msg_id, sat_id);
-    }
-
-    trace(2, "Unsupported raw navigation message: %s %s\n", gnss, signal_type);
-    return -1;
+    return status;
 }
 
 static int decode_ogrp_timestamp(raw_t *raw, json_object *jobj) {
     json_object* timestamp;
     json_type type;
+    double utc_offset, gps_leap_sec;
 
     trace(5,"decode_ogrp_timestamp:\n");
 
@@ -449,14 +373,18 @@ static int decode_ogrp_timestamp(raw_t *raw, json_object *jobj) {
         return -1;
     }
 
+    gps_leap_sec = 17.0;
+    /* Time between GPS start (05.01.1980) and 01.01.1970
+     * 432000.0 -> 5 days; 315532800.0 -> 10 years */
+    utc_offset = 432000.0 + 315532800.0 - gps_leap_sec;
     raw->time = epoch2time(gpst0);
     type = json_object_get_type(timestamp);
     switch (type) {
     case json_type_int:
-        raw->time.time += json_object_get_int(timestamp);
+        raw->time.time += (json_object_get_int(timestamp) - (int)utc_offset);
         break;
     case json_type_double:
-        raw->time.time += json_object_get_double(timestamp);
+        raw->time.time += (json_object_get_double(timestamp) - utc_offset);
         raw->time.sec = json_object_get_double(timestamp) - (int)json_object_get_double(timestamp);
         break;
     case json_type_string:
@@ -491,8 +419,8 @@ static int decode_ogrp_msg(raw_t *raw) {
     if (decode_ogrp_timestamp(raw, jobj) != 1) return -1;
 
     id_value_str = json_object_get_string(id_value);
-    if (strcmp(id_value_str, "measurement") == 0) return decode_ogrp_measurement(raw, jobj);
-    if (strcmp(id_value_str, "raw_nav_msg") == 0) return decode_ogrp_raw_nav_msg(raw, jobj);
+    if (strcmp(id_value_str, "channel_measurements") == 0) return decode_ogrp_channel_measurements(raw, jobj);
+    if (strcmp(id_value_str, "ephemeris") == 0) return decode_ogrp_ephemeris(raw, jobj);
     /* TODO implement other message types */
     else
     {
