fc82124caafb6fd294cf66c7c9def64f3037f867
Neelam Kumari <neelam.kumari@iis.fraunhofer.de>
2016-02-08 09:52:32 +0100

    rtklib: util: Script to convert rtkEngOutput to rtklib compatible output
    Change-Id: If43708da4a7111735efca4aa2eeccb779cd70a43

---
+++ util/rtkEngineOut.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import jsonpickle
+
+
+def main():
+    mode = 5
+    while 1:
+        l = sys.stdin.readline()
+        pos = jsonpickle.decode(l)
+        if pos.has_key('pvtOutput'):
+            pvt = pos['pvtOutput']
+        rover = pos['pvtOutput']['rovPos']['pos']
+        if (pvt['ambmode'] == 'Fixed'):
+            mode = 1
+            print('%s %d %f %f %f %d %d 0 0 0 0 0 0 0 -1' % ('2016/02/04', pvt['time'],rover['x'],rover['y'], rover['z'], mode, pvt['nSats']))
+
+if __name__ == '__main__':
+    main()
