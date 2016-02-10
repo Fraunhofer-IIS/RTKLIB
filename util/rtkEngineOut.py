9363e787c69857e0d1370707d9cea0e4dd0c50ad
Christian Strobel <christian.strobel@iis.fraunhofer.de>
2016-02-10 15:20:59 +0100

    util: rtkEngineOut: Improve error handling
    Change-Id: I299b02c041fff7c079e5f48a5820997aa26f27f7

--- util/rtkEngineOut.py
+++ util/rtkEngineOut.py
@@ -5,16 +5,27 @@ import jsonpickle
 
 
 def main():
-    mode = 5
-    while 1:
+    while True:
         l = sys.stdin.readline()
-        pos = jsonpickle.decode(l)
-        if 'pvtOutput' in pos:
-            pvt = pos['pvtOutput']
-        rover = pos['pvtOutput']['rovPos']['pos']
-        if (pvt['ambmode'] == 'Fixed'):
-            mode = 1
-            print('%s %d %f %f %f %d %d 0 0 0 0 0 0 0 -1' % ('2016/02/04', pvt['time'], rover['x'], rover['y'], rover['z'], mode, pvt['nSats']))
+        try:
+            pos = jsonpickle.decode(l)
+        except ValueError:
+            continue
+        if 'pvtOutput' not in pos:
+            continue
+        pvt = pos['pvtOutput']
+        if 'roverPos' not in pvt or 'time' not in pvt or 'nSats' not in pvt:
+            continue
+        if 'pos' not in pvt['roverPos']:
+            continue
+        rover = pvt['rovPos']['pos']
+        mode = 5  # Default single point position
+        if 'ambmode' in pvt:
+            if (pvt['ambmode'] == 'Fixed'):
+                mode = 1
+        if 'x' not in rover or 'y' not in rover or 'z' not in rover:
+            continue
+        print('%s %d %f %f %f %d %d 0 0 0 0 0 0 0 -1' % ('2016/02/04', pvt['time'], rover['x'], rover['y'], rover['z'], mode, pvt['nSats']))
 
 if __name__ == '__main__':
     main()
