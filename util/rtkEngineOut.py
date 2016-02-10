f2862ae8e1087bedf9e41228b6ad640ba07f8df2
Christian Strobel <christian.strobel@iis.fraunhofer.de>
2016-02-10 15:05:04 +0100

    utils: rtkEngineOut: Fix pep8 warnings
    Change-Id: I7b5499ba58c8e1ccc3708e49b2a9689154938d00

--- util/rtkEngineOut.py
+++ util/rtkEngineOut.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import time
 import jsonpickle
 
 
@@ -10,12 +9,12 @@ def main():
     while 1:
         l = sys.stdin.readline()
         pos = jsonpickle.decode(l)
-        if pos.has_key('pvtOutput'):
+        if 'pvtOutput' in pos:
             pvt = pos['pvtOutput']
         rover = pos['pvtOutput']['rovPos']['pos']
         if (pvt['ambmode'] == 'Fixed'):
             mode = 1
-            print('%s %d %f %f %f %d %d 0 0 0 0 0 0 0 -1' % ('2016/02/04', pvt['time'],rover['x'],rover['y'], rover['z'], mode, pvt['nSats']))
+            print('%s %d %f %f %f %d %d 0 0 0 0 0 0 0 -1' % ('2016/02/04', pvt['time'], rover['x'], rover['y'], rover['z'], mode, pvt['nSats']))
 
 if __name__ == '__main__':
     main()
